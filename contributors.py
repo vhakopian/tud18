81aa72f9e0869f162061dd361c6504fea1345a50
vhakopian <vahagn.hakopian@gmail.com>
2018-03-21 12:49:55 +0100

    size and churn

--- contributors.py
+++ contributors.py
@@ -2,37 +2,59 @@ from git import *
 import datetime
 
 
-EMPTY_TREE_SHA   = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
+
 
 def get_dico(repo_path):
     repo = Repo(repo_path)
 
     commits  = repo.iter_commits()
     dico = {}
+    dico_sizechurn = {}
     c=0
+    
     for commit in repo.iter_commits('v4-dev'):
-    #    print(commit.committed_date)
+        
         year = int(datetime.datetime.fromtimestamp(commit.committed_date).strftime('%Y'))
         if(year >2016):
-
             continue
         c+=1
         contributor_name = commit.author.name
-        parent = commit.parents[0] if commit.parents else EMPTY_TREE_SHA
-        #print(contributor_name)
-        for item in commit.diff(parent):
-            file_name = item.a_path
-        #    print(file_name)
+        
+        changes = commit.stats.files
+        
+        for file in changes:
+            
+            file_name = str(file)
+            churn = changes[file]['deletions']+changes[file]['insertions']
+            
             if dico.has_key(file_name):
                 if dico[file_name].has_key(contributor_name):
                     dico[file_name][contributor_name] += 1
-                else: dico[file_name][contributor_name] = 1
+                else:
+                    dico[file_name][contributor_name] = 1
             else:
                 dico[file_name] = {contributor_name: 1}
-
-#        print(c)
-#    print(dico)
-    return(dico)
+            
+            if dico_sizechurn.has_key(file_name):
+                dico_sizechurn[file_name]["churn"] += churn
+            else :
+                dico_sizechurn[file_name] = {"churn" : churn, "size" : 0}
+        print(c)
+     
+  
+    for filename in dico_sizechurn:
+        try:
+            size = 0
+            for commit, lines in repo.blame('HEAD', filename):
+                size += len(lines)
+            dico_sizechurn[filename]["size"]=size
+            
+            print(size)
+            
+        except :
+            pass 
+            
+    return(dico, dico_sizechurn)
 
 
 def metrics(dico):
@@ -58,21 +80,23 @@ def metrics(dico):
 
 
 
-def write_results(dict):
+def write_results(dict, dict_sizechurn):
 
     file  = open("results.tsv", "w")
-    file.write("filename\tminor\tmajor\ttotal\townership\n")
+    file.write("filename\tminor\tmajor\ttotal\townership\tsize\tchurn\n")
     for filename in dict:
         d = dict[filename]
-        file.write(filename + "\t" + str(d['minor'])+"\t"+str(d['major'])+"\t"+str(d['total'])+"\t"+str(d['ownership'])+ "\n")
+        d_sizechurn = dict_sizechurn[filename]
+
+        file.write(filename + "\t" + str(d['minor'])+"\t"+str(d['major'])+"\t"+str(d['total'])+"\t"+str(d['ownership'])+"\t"+str(d_sizechurn['size'])+"\t"+str(d_sizechurn['churn'])+ "\n")
 
     file.close()
 
 def main():
-    path = "/Users/danielmendonca/git/bootstrap"
-    dico = get_dico(path)
+    path = "/Users/vahagn/bootstrap"
+    dico, dico_sizechurn = get_dico(path)
     m = metrics(dico)
-    write_results(m)
+    write_results(m, dico_sizechurn)
 
 
 if __name__ == "__main__":
